93ff82e44b145709118ba34f751cc34a5c759c4d
Tinh Bui Duong <bdtinh1999@gmail.com>
2024-11-01 20:13:21 +0700

    Initial commit for Deploy_to_VMSS branch

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -1,104 +1,109 @@
 from flask import Flask, request, render_template
 import os
-import random
 import redis
 import socket
-import sys
 import logging
-from datetime import datetime
+from opencensus.ext.azure.log_exporter import AzureLogHandler
+from opencensus.ext.azure import metrics_exporter
+from opencensus.ext.azure.trace_exporter import AzureExporter
+from opencensus.ext.flask.flask_middleware import FlaskMiddleware
+from opencensus.trace.samplers import ProbabilitySampler
+from opencensus.trace.tracer import Tracer
 
-# App Insights
-# TODO: Import required libraries for App Insights
+# Azure Instrumentation Key
+instrumentation_key = os.getenv('INSTRUMENTATION_KEY', 'e1927438-a67f-4261-bc7e-bd60c06846fa')
 
-# Logging
-logger = # TODO: Setup logger
-
-# Metrics
-exporter = # TODO: Setup exporter
-
-# Tracing
-tracer = # TODO: Setup tracer
-
+# Initialize Flask application
 app = Flask(__name__)
 
-# Requests
-middleware = # TODO: Setup flask middleware
+# Configure logging with Azure Log Handler
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+handler = AzureLogHandler(connection_string=f'InstrumentationKey={instrumentation_key}')
+logger.addHandler(handler)
 
-# Load configurations from environment or config file
+# Set up metrics exporter for application insights
+metrics_exporter_instance = metrics_exporter.new_metrics_exporter(
+    enable_standard_metrics=True,
+    connection_string=f'InstrumentationKey={instrumentation_key}'
+)
+
+# Configure tracing with Azure
+tracer = Tracer(
+    exporter=AzureExporter(connection_string=f'InstrumentationKey={instrumentation_key}'),
+    sampler=ProbabilitySampler(rate=1.0),
+)
+
+# Middleware for OpenCensus
+FlaskMiddleware(
+    app,
+    exporter=AzureExporter(connection_string=f'InstrumentationKey={instrumentation_key}'),
+    sampler=ProbabilitySampler(rate=1.0),
+)
+
+# Load configuration from a file or environment variables
 app.config.from_pyfile('config_file.cfg')
 
-if ("VOTE1VALUE" in os.environ and os.environ['VOTE1VALUE']):
-    button1 = os.environ['VOTE1VALUE']
-else:
-    button1 = app.config['VOTE1VALUE']
+# Read values for buttons and title from environment variables or config
+button1 = os.getenv('VOTE1VALUE', app.config['VOTE1VALUE'])
+button2 = os.getenv('VOTE2VALUE', app.config['VOTE2VALUE'])
+title = os.getenv('TITLE', app.config['TITLE'])
 
-if ("VOTE2VALUE" in os.environ and os.environ['VOTE2VALUE']):
-    button2 = os.environ['VOTE2VALUE']
-else:
-    button2 = app.config['VOTE2VALUE']
+# Initialize Redis connection
+redis_client = redis.Redis()
 
-if ("TITLE" in os.environ and os.environ['TITLE']):
-    title = os.environ['TITLE']
-else:
-    title = app.config['TITLE']
-
-# Redis Connection
-r = redis.Redis()
-
-# Change title to host name to demo NLB
-if app.config['SHOWHOST'] == "true":
+# Set hostname as title for NLB demo if configured
+if app.config.get('SHOWHOST') == "true":
     title = socket.gethostname()
 
-# Init Redis
-if not r.get(button1): r.set(button1,0)
-if not r.get(button2): r.set(button2,0)
+# Initialize Redis keys for voting buttons if not already set
+if not redis_client.get(button1): 
+    redis_client.set(button1, 0)
+if not redis_client.get(button2): 
+    redis_client.set(button2, 0)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-
     if request.method == 'GET':
+        # Fetch current vote counts
+        vote1 = redis_client.get(button1).decode('utf-8')
+        vote2 = redis_client.get(button2).decode('utf-8')
 
-        # Get current values
-        vote1 = r.get(button1).decode('utf-8')
-        # TODO: use tracer object to trace cat vote
-        vote2 = r.get(button2).decode('utf-8')
-        # TODO: use tracer object to trace dog vote
+        # Trace votes
+        with tracer.span(name="Cats_vote"):
+            pass  # Add any specific tracing logic if needed
+        
+        with tracer.span(name="Dogs_vote"):
+            pass  # Add any specific tracing logic if needed
 
-        # Return index with values
+        # Render the template with current votes
         return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
 
     elif request.method == 'POST':
-
         if request.form['vote'] == 'reset':
+            # Reset vote counts
+            redis_client.set(button1, 0)
+            redis_client.set(button2, 0)
 
-            # Empty table and return results
-            r.set(button1,0)
-            r.set(button2,0)
-            vote1 = r.get(button1).decode('utf-8')
-            properties = {'custom_dimensions': {'Cats Vote': vote1}}
-            # TODO: use logger object to log cat vote
-
-            vote2 = r.get(button2).decode('utf-8')
-            properties = {'custom_dimensions': {'Dogs Vote': vote2}}
-            # TODO: use logger object to log dog vote
+            # Log reset votes
+            vote1 = redis_client.get(button1).decode('utf-8')
+            logger.info('Cat vote reset', extra={'custom_dimensions': {'Cats Vote': vote1}})
+            vote2 = redis_client.get(button2).decode('utf-8')
+            logger.info('Dog vote reset', extra={'custom_dimensions': {'Dogs Vote': vote2}})
 
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
-
         else:
-
-            # Insert vote result into DB
+            # Increment the vote count
             vote = request.form['vote']
-            r.incr(vote,1)
+            redis_client.incr(vote, 1)
 
-            # Get current values
-            vote1 = r.get(button1).decode('utf-8')
-            vote2 = r.get(button2).decode('utf-8')
+            # Fetch updated vote counts
+            vote1 = redis_client.get(button1).decode('utf-8')
+            vote2 = redis_client.get(button2).decode('utf-8')
 
-            # Return results
+            # Return updated results
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
 
 if __name__ == "__main__":
-    # TODO: Use the statement below when running locally
-    app.run() 
-    # TODO: Use the statement below before deployment to VMSS
-    # app.run(host='0.0.0.0', threaded=True, debug=True) # remote
+    app.run(debug=True)  # Use debug=True for local development
+    # app.run(host='0.0.0.0', threaded=True)  # Uncomment for deployment
